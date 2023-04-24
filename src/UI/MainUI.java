4d019ba409be4f6d728c898d19bc3dcd436bb059
Heitor Souza <heitor.sf5@gmail.com>
2023-04-24 18:37:17 -0300

    implementado classe Principal com create e getAll + populando a lista de funcionarios e a tabela com esses metodos

--- src/UI/MainUI.java
+++ src/UI/MainUI.java
@@ -1,9 +1,6 @@
 package UI;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainUI {
@@ -16,33 +13,20 @@ public class MainUI {
 
     public MainUI() {
         createTable();
-        initiPrinci();
-        myFuncis();
     }
 
     public JPanel getRootPanel() { return rootPanel; };
 
     public void createTable() {
+        Principal principal = new Principal();
+        List<Funcionario> lista_funcionarios = principal.getAll();
+        Object[][] data = new Object [lista_funcionarios.size()][]; // converte Lista<Funcionario> para um "array 2D" assim como necessario para DefaultTableModel
+        for (int i = 0; i < lista_funcionarios.size(); i++) {
+            data[i] = lista_funcionarios.get(i).getAttributes();
+        }
         funcionarios.setModel(new DefaultTableModel(
-                myFuncis(), // cells
+                data, // cells
                 new String[]{"Nome", "Data Nascimento", "Salário", "Função"} // headers
         ));
-    }
-
-    private Object[][] myFuncis() {
-        List<Funcionario> princi = initiPrinci().getAllFunci();
-        Object[][] allFuncis = princi.stream().map(Funcionario::getAttributes).toArray(Object[][]::new); // Equivalente à um .map() do JavaScript
-        return allFuncis;
-    }
-
-    public Principal initiPrinci() {
-        Principal princi = new Principal();
-        Funcionario funci1 = new Funcionario("Camille", LocalDate.of(1993, 12, 31), new BigDecimal("2500.45"), "Operador");
-        Funcionario funci2 = new Funcionario("Char", LocalDate.of(1974, 4, 9), new BigDecimal("8000.00"), "Gerente");
-        Funcionario funci3 = new Funcionario("Reccoa", LocalDate.of(1984, 8, 10), new BigDecimal("4209.99"), "Diretor");
-        Funcionario funci4 = new Funcionario("Amuro", LocalDate.of(1989, 11, 30), new BigDecimal("5040.03"), "Recepcionista");
-        List<Funcionario> listFunci = Arrays.asList(funci1, funci2, funci3, funci4);
-        princi.setAllFunci(listFunci);
-        return princi;
     }
 }
--- src/UI/Principal.java
+++ src/UI/Principal.java
@@ -1,32 +1,34 @@
-package UI;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
+        package UI;
+        import UI.Funcionario;
+        import java.math.BigDecimal;
+        import java.time.LocalDate;
+        import java.util.ArrayList;
+        import java.util.List;
 
-public class Principal {
-    private List<Funcionario> allFunci;
+        public class Principal {
 
-    public void setAllFunci(List<Funcionario> allFunci) {
-        this.allFunci = allFunci;
-    } // create All
+            private List<Funcionario> funcionarios;
 
-    public List<Funcionario> getAllFunci() {
-        return allFunci;
-    } // get All
+            public Principal() {
+                funcionarios = new ArrayList<>();
+                create("Camille", LocalDate.of(1993, 12, 31), new BigDecimal("2500.45"), "Operador");
+                create("Char", LocalDate.of(1974, 4, 9), new BigDecimal("8000.00"), "Gerente");
+                create("Reccoa", LocalDate.of(1984, 8, 10), new BigDecimal("4209.99"), "Diretor");
+                create("Amuro", LocalDate.of(1989, 11, 30), new BigDecimal("5040.03"), "Recepcionista");
+            }
 
-    public void addOneFunci(Funcionario funci) {
-        this.allFunci.add(funci);
-    } // create One
+            public void create(String nome, LocalDate dataNasc, BigDecimal salario, String funcao) {
+                Funcionario func = new Funcionario(nome, dataNasc, salario, funcao);
+                funcionarios.add(func);
+            }
 
-    public void deleteFunci(String nome) {
-//        List<Funcionario> newList = this.getAllFunci();
-//        newList.remove(nome);
-//        this.setAllFunci(newList);
-        this.allFunci.remove(nome);
-//        Não funciona como espero...
-    } // delete One
-}
+            public List<Funcionario> getAll() {
+                return funcionarios;
+            }
 
-// Não consegui fazer as mudanças na minha lista de Funcionarios ser refletida na tabela, então fiquei com dificuldade de implementar o resto das funcionalidades desta classe..
+//            Funcionario funci1 = new Funcionario("Camille", LocalDate.of(1993, 12, 31), new BigDecimal("2500.45"), "Operador");
+//            Funcionario funci2 = new Funcionario("Char", LocalDate.of(1974, 4, 9), new BigDecimal("8000.00"), "Gerente");
+//            Funcionario funci3 = new Funcionario("Reccoa", LocalDate.of(1984, 8, 10), new BigDecimal("4209.99"), "Diretor");
+//            Funcionario funci4 = new Funcionario("Amuro", LocalDate.of(1989, 11, 30), new BigDecimal("5040.03"), "Recepcionista");
 
-// Minha ideia após remediar isso é implementar as outras funcionalidades de Principal em formato de botões e text-fields no próprio Frame
+        }
